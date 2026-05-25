2219705f03ae1550b7651060b6a981e59e144647
Hasnain <aq9506@gmail.com>
2026-05-25 10:54:03 +0530

    warmup added

--- ecommerce-frontend/src/pages/Home.jsx
+++ ecommerce-frontend/src/pages/Home.jsx
@@ -40,6 +40,34 @@ export default function Home() {
         if (e.key === "Enter") searchProducts();
     };
 
+     useEffect(() => {
+           
+            const toastId = toast.loading("Server is spinning up...");
+            const warmUp = async () => {
+                
+                try {
+                    const response = await publicApi.get("/auth/warmup");
+                    if (response.status === 200) {
+                        toast.update(toastId, {
+                            render: "Server is Online 🎉",
+                            type: "success",
+                            isLoading: false,
+                            autoClose: 3000,
+                        });
+                    }
+                } catch (err) {
+                    console.error("Warmup failed:", err);
+                    toast.update(toastId, {
+                        render: "Something went wrong",
+                        type: "error",
+                        isLoading: false,
+                        autoClose: 3000,
+                    });
+                }
+            };
+            warmUp();
+        }, []);
+
     useEffect(() => {
         fetchProducts();
     }, []);
